596bc897a75e2161b8d531811208826e665e7c6c
luunhungoc <ngoc.luunhu@gmail.com>
2024-08-28 15:21:54 +0700

    Initial commit

---
+++ src/test/java/demo/example/demo/Day10SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day10SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
